e361f4d14d294c28d7a3791fe201e75eeaafa237
Nich Galzin <nichgalzin@gmail.com>
2024-10-17 14:39:12 +0100

    refactor: deletes audience member content type
    Relates #22

--- src/api/audience-member/routes/audience-member.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * audience-member router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::audience-member.audience-member');
--- src/api/audience-member/services/audience-member.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * audience-member service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::audience-member.audience-member');
