0d8ed1e16212a82b700d7abd89b2770720ae2cba
Nich Galzin <nichgalzin@gmail.com>
2024-11-01 17:13:16 +0000

    makes do manager conditional for production

--- config/plugins.js
+++ config/plugins.js
@@ -1,32 +1,42 @@
-module.exports = ({ env }) => ({
-  transformer: {
-    enabled: true,
-    config: {
-      responseTransforms: {
-        removeAttributesKey: true,
-        removeDataKey: true,
-      },
-      requestTransforms: {
-        wrapBodyWithDataKey: true,
-      },
-    },
-  },
-  "user-permissions": {
-    config: {
-      jwt: {
-        expiresIn: "128d",
+module.exports = ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+
+  const config = {
+    transformer: {
+      enabled: true,
+      config: {
+        responseTransforms: {
+          removeAttributesKey: true,
+          removeDataKey: true,
+        },
+        requestTransforms: {
+          wrapBodyWithDataKey: true,
+        },
       },
     },
-  },
-  upload: {
-    config: {
-      provider: "strapi-provider-upload-do", 
-      providerOptions: {
-        key: env('DO_SPACE_ACCESS_KEY'),
-        secret: env('DO_SPACE_SECRET_KEY'),
-        endpoint: env('DO_SPACE_ENDPOINT'),
-        space: env('DO_SPACE_BUCKET'),
-      }
+    "user-permissions": {
+      config: {
+        jwt: {
+          expiresIn: "128d",
+        },
+      },
     },
-  }, 
-});
+  };
+
+  // Only add Digital Ocean upload config in production
+  if (isProduction) {
+    config.upload = {
+      config: {
+        provider: "strapi-provider-upload-do", 
+        providerOptions: {
+          key: env('DO_SPACE_ACCESS_KEY'),
+          secret: env('DO_SPACE_SECRET_KEY'),
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          space: env('DO_SPACE_BUCKET'),
+        }
+      },
+    };
+  }
+
+  return config;
+};
