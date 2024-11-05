9298dc353e0526b62b135c24def4dc8f8ea6ed7c
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-11-05 10:43:03 +0000

    fix: reference env values with imported env function

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,6 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
+module.exports = ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
   {
     name: "strapi::security",
     config: {
@@ -11,54 +11,62 @@ module.exports = [
             "'self'",
             "https:",
             "chrome-extension://*",
-            "moz-extension://*"
+            "moz-extension://*",
           ],
           "img-src": [
             "'self'",
             "data:",
             "blob:",
-            "{DO_SPACE_BUCKET}.s3.{DO_SPACE_REGION}.amazonaws.com",
-            "https://{DO_SPACE_BUCKET}.s3.{DO_SPACE_REGION}.amazonaws.com",
+            `${env("DO_SPACE_BUCKET")}.s3.${env(
+              "DO_SPACE_REGION"
+            )}.amazonaws.com`,
+            `https://${env("DO_SPACE_BUCKET")}.s3.${env(
+              "DO_SPACE_REGION"
+            )}.amazonaws.com`,
             "chrome-extension://*",
-            "moz-extension://*"
+            "moz-extension://*",
           ],
           "media-src": [
             "'self'",
             "data:",
             "blob:",
-            "{DO_SPACE_BUCKET}.s3.{DO_SPACE_REGION}.amazonaws.com",
-            "https://{DO_SPACE_BUCKET}.s3.{DO_SPACE_REGION}.amazonaws.com",
+            `${env("DO_SPACE_BUCKET")}.s3.${env(
+              "DO_SPACE_REGION"
+            )}.amazonaws.com`,
+            `https://${env("DO_SPACE_BUCKET")}.s3.${env(
+              "DO_SPACE_REGION"
+            )}.amazonaws.com`,
             "chrome-extension://*",
-            "moz-extension://*"
+            "moz-extension://*",
           ],
           "default-src": [
             "'self'",
             "chrome-extension://*",
-            "moz-extension://*"
+            "moz-extension://*",
           ],
           "script-src": [
             "'self'",
             "'unsafe-inline'",
             "chrome-extension://*",
-            "moz-extension://*"
+            "moz-extension://*",
           ],
           upgradeInsecureRequests: null,
         },
       },
       cors: {
         enabled: true,
-        origin: ['chrome-extension://*', 'moz-extension://*'],
+        origin: ["chrome-extension://*", "moz-extension://*"],
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept']
-      }
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+        headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
