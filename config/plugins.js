6bbf35f0b37ed2b89992c66a42e39b415df4cc06
Nich Galzin <nichgalzin@gmail.com>
2024-11-05 15:10:51 +0000

    reverts back to strapi upload do for upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -21,28 +21,15 @@ module.exports = ({ env }) => {
     },
     upload: {
       config: {
-        provider: "aws-s3",
+        provider: "strapi-provider-upload-do", 
         providerOptions: {
-          credentials: {
-            accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-            secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-          },
-          region: "lon1",
-          endpoint: env("DO_SPACE_ENDPOINT"),
-          params: {
-            Bucket: env("DO_SPACE_BUCKET"),
-          },
-          forcePathStyle: true,
-        },
-        actionOptions: {
-          upload: {
-            ACL: null,
-          },
-          uploadStream: {
-            ACL: null,
-          },
-          delete: {},
-        },
+          key: env('DO_SPACE_ACCESS_KEY'),
+          secret: env('DO_SPACE_SECRET_KEY'),
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          space: env('DO_SPACE_BUCKET'),
+          directory: env('DO_SPACE_DIRECTORY'),
+          cdn: env('DO_SPACE_CDN'),
+        }
       },
     },
   };
