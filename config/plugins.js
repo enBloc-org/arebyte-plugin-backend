75c2eabdbf1d0e0ac9641bea0c357deee9a38993
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-11-05 13:19:01 +0000

    build: add explicit ACL publish and read permissions

--- config/plugins.js
+++ config/plugins.js
@@ -31,14 +31,15 @@ module.exports = ({ env }) => {
           endpoint: env("DO_SPACE_ENDPOINT"),
           params: {
             Bucket: env("DO_SPACE_BUCKET"),
+            ACL: "publish/read",
           },
         },
         actionOptions: {
           upload: {
-            ACL: null,
+            ACL: "publish/read",
           },
           uploadStream: {
-            ACL: null,
+            ACL: "publish/read",
           },
           delete: {},
         },
