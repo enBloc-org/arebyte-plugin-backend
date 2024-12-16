1ca5276a84486f01a7fce9b465e40b7aa21a01c0
Nich Galzin <nichgalzin@gmail.com>
2024-12-16 10:46:21 +0000

    feat:config for nodemailer provider
    Relates #57

--- config/plugins.js
+++ config/plugins.js
@@ -21,29 +21,39 @@ module.exports = ({ env }) => {
     },
     email: {
       config: {
-        provider: "sendgrid",
+        provider: 'nodemailer',
         providerOptions: {
-          apiKey: env("SENDGRID_API_KEY"),
+          host: env('SMTP_HOST'),
+          port: env('SMTP_PORT'),
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          secure: false,
+          requireTLS: true,
+          tls: {
+            rejectUnauthorized: false
+          }
         },
         settings: {
-          defaultFrom: env("AREBYTE_EMAIL"),
-          defaultReplyTo: env("AREBYTE_EMAIL"),
+          defaultFrom: env('EMAIL_FROM'),
+          defaultReplyTo: env('EMAIL_REPLY_TO'),
         },
       },
     },
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-do",
-        providerOptions: {
-          key: env('DO_SPACE_ACCESS_KEY'),
-          secret: env('DO_SPACE_SECRET_KEY'),
-          endpoint: env('DO_SPACE_ENDPOINT'),
-          space: env('DO_SPACE_BUCKET'),
-          directory: env('DO_SPACE_DIRECTORY'),
-          cdn: env('DO_SPACE_CDN'),
-        }
-      },
-    },
+  //   upload: {
+  //     config: {
+  //       provider: "strapi-provider-upload-do",
+  //       providerOptions: {
+  //         key: env('DO_SPACE_ACCESS_KEY'),
+  //         secret: env('DO_SPACE_SECRET_KEY'),
+  //         endpoint: env('DO_SPACE_ENDPOINT'),
+  //         space: env('DO_SPACE_BUCKET'),
+  //         directory: env('DO_SPACE_DIRECTORY'),
+  //         cdn: env('DO_SPACE_CDN'),
+  //       }
+  //     },
+  //   },
   };
 
   return config;
