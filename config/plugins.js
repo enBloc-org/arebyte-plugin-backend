9aa320cb4d0b4d47d5737362377b0e9ef400ba0a
Nich Galzin <nichgalzin@gmail.com>
2025-01-13 10:56:07 +0000

    fix: uncomments upload do
    Relate #57

--- config/plugins.js
+++ config/plugins.js
@@ -21,39 +21,39 @@ module.exports = ({ env }) => {
     },
     email: {
       config: {
-        provider: 'nodemailer',
+        provider: "nodemailer",
         providerOptions: {
-          host: env('SMTP_HOST'),
-          port: env('SMTP_PORT'),
+          host: env("SMTP_HOST"),
+          port: env("SMTP_PORT"),
           auth: {
-            user: env('SMTP_USERNAME'),
-            pass: env('SMTP_PASSWORD'),
+            user: env("SMTP_USERNAME"),
+            pass: env("SMTP_PASSWORD"),
           },
           secure: false,
           requireTLS: true,
           tls: {
-            rejectUnauthorized: false
-          }
+            rejectUnauthorized: false,
+          },
         },
         settings: {
-          defaultFrom: env('EMAIL_FROM'),
-          defaultReplyTo: env('EMAIL_REPLY_TO'),
+          defaultFrom: env("EMAIL_FROM"),
+          defaultReplyTo: env("EMAIL_REPLY_TO"),
         },
       },
     },
-  //   upload: {
-  //     config: {
-  //       provider: "strapi-provider-upload-do",
-  //       providerOptions: {
-  //         key: env('DO_SPACE_ACCESS_KEY'),
-  //         secret: env('DO_SPACE_SECRET_KEY'),
-  //         endpoint: env('DO_SPACE_ENDPOINT'),
-  //         space: env('DO_SPACE_BUCKET'),
-  //         directory: env('DO_SPACE_DIRECTORY'),
-  //         cdn: env('DO_SPACE_CDN'),
-  //       }
-  //     },
-  //   },
+    upload: {
+      config: {
+        provider: "strapi-provider-upload-do",
+        providerOptions: {
+          key: env("DO_SPACE_ACCESS_KEY"),
+          secret: env("DO_SPACE_SECRET_KEY"),
+          endpoint: env("DO_SPACE_ENDPOINT"),
+          space: env("DO_SPACE_BUCKET"),
+          directory: env("DO_SPACE_DIRECTORY"),
+          cdn: env("DO_SPACE_CDN"),
+        },
+      },
+    },
   };
 
   return config;
